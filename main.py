6ead481d0cf9140608ab3a3983b9b657be5328d9
Bad Weather <dale@distract.org>
2019-05-25 12:42:47 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,39 +4,15 @@ from bs4 import BeautifulSoup
 from datetime import datetime, date
 
 # Realm names
-realmA = "Light's Hope"
-realmB = "Skywall"
-realmC = "Wrathbringer"
+realmA = "realmA"
+realmB = "realmB"
+realmC = "realmC"
 
 # Raiders
 raiders = [
-    { "name": "Darc", "realm": realmA },
-    { "name": "龍蝦三爭霸", "realm": realmA },
-    { "name": "喔啊啊布德爾", "realm": realmC },
-    { "name": "阿強號", "realm": realmA },
-    { "name": "香茅千層蛋糕", "realm": realmA },
-    { "name": "沙發破一個洞", "realm": realmA },
-    { "name": "緋月淮星", "realm": realmA },
-    { "name": "妹妹大人", "realm": realmB },
-    { "name": "快來踩我", "realm": realmB },
-    { "name": "一灰羽一", "realm": realmA },
-    { "name": "Yoyoman", "realm": realmB },
-    { "name": "嬌穴組長", "realm": realmB },
-    { "name": "貓語者", "realm": realmB },
-    { "name": "Seraphen", "realm": realmA },
-    { "name": "撞牆當練功", "realm": realmA },
-    { "name": "闇之咒術師", "realm": realmA },
-    { "name": "江城子", "realm": realmA },
-    { "name": "伊蘇露", "realm": realmA },
-    { "name": "絕緋", "realm": realmA },
-    { "name": "阿魔達", "realm": realmA },
-    { "name": "Yinuo", "realm": realmB },
-    { "name": "性感大蘿蔔", "realm": realmA },
-    { "name": "一塊錢的功德", "realm": realmB },
-    { "name": "開朗小露娜", "realm": realmA },
-    { "name": "安琪莉可可", "realm": realmB },
-    { "name": "遺忘賽德勒斯", "realm": realmA },
-    { "name": "Latina", "realm": realmA },
+    { "name": "raiderD", "realm": realmA },
+    { "name": "raiderE", "realm": realmB },
+    { "name": "raiderF", "realm": realmC },
 ]
 
 print("\n" + "Checking raider.io data..." + "\n")
